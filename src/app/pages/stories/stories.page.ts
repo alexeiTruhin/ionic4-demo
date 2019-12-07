0e4175cda7d05e3c3508bc506887582f7206a08d
alexeiTruhin <alexei.truhin@gmail.com>
2019-12-08 00:05:25 +0200

    feat(stories): display dummy stories from service

--- src/app/pages/stories/stories.page.ts
+++ src/app/pages/stories/stories.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Story } from './story.model';
+import { StoriesService } from './stories.service';
+
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.page.html',
   styleUrls: ['./stories.page.scss'],
 })
 export class StoriesPage implements OnInit {
-
-  constructor() { }
+  stories: Story[];
+  constructor(private storiesService: StoriesService) { }
 
   ngOnInit() {
+    this.stories = this.storiesService.getStories();
   }
 
 }
