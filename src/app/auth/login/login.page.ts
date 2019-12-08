60068f273efbdaac27f2d93df0fa70054b341258
alexeiTruhin <alexei.truhin@gmail.com>
2019-12-09 00:24:47 +0200

    feat(auth): validate login input on front-end

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AuthenticationService } from '../authentication.service';
 
@@ -8,13 +9,31 @@ import { AuthenticationService } from '../authentication.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
 
-  constructor(private authService: AuthenticationService) { }
+  constructor(
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
-  login() {
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     this.authService.login();
   }
 
