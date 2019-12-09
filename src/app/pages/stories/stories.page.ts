da0541897de0a47543b260ad0c622d75996a2582
alexeiTruhin <alexei.truhin@gmail.com>
2019-12-09 20:52:04 +0200

    feat(stories): add basic loading controller

--- src/app/pages/stories/stories.page.ts
+++ src/app/pages/stories/stories.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 import { Story } from './story.model';
 import { StoriesService } from './stories.service';
@@ -10,12 +11,32 @@ import { StoriesService } from './stories.service';
 })
 export class StoriesPage implements OnInit {
   stories: Story[];
-  constructor(private storiesService: StoriesService) { }
+
+  constructor(
+    private storiesService: StoriesService,
+    public loadingController: LoadingController
+  ) {
+    
+  }
 
   ngOnInit() {
+    this.showLoader();
     this.storiesService.getStories().subscribe( stories => {
       this.stories = stories;
+      this.hideLoader();
     });
+  }
+
+  showLoader() {
+    this.loadingController.create({
+      message: 'Loading content...'
+    }).then((res) => {
+      res.present();
+    });
+  }
+
+  hideLoader() {
+    this.loadingController.dismiss();
   }
 
 }
